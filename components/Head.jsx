ab681f1a3aa8323c2b6713e3a63b12d000a7ccea
nghiepit <me@nghiepit.dev>
2020-02-09 22:53:13 +0700

    chore(typos): default description, thumb image

--- components/Head.jsx
+++ components/Head.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import NextHead from 'next/head';
 
-const Head = ({title, children}) => {
+const Head = ({children, title, description, thumb}) => {
   return (
     <NextHead>
       <title>{title}</title>
+      <meta key="description" name="description" content={description} />
+      <meta key="og:image" property="og:image" content={thumb} />
       {children}
     </NextHead>
   );
@@ -13,10 +15,15 @@ const Head = ({title, children}) => {
 
 Head.propTypes = {
   title: PropTypes.string,
+  description: PropTypes.string,
+  thumb: PropTypes.string,
 };
 
 Head.defaultProps = {
   title: 'GraphQL Next with love Botble',
+  description:
+    'Built with GraphQL + Next.js. Botble CMS based on Laravel Framework',
+  thumb: 'https://drive.google.com/uc?id=1FROB08kEk5LyVZlBorEt3zsFOIDykRjD',
 };
 
 export default Head;
